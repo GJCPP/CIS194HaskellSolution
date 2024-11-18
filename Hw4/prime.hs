8fa344754d32677ebb284662b2aedaacfdeb0588
1176850634@qq.com <1176850634@qq.com>
2024-11-18 22:24:23 +0800

    Hw4

--- Hw4/prime.hs
+++ Hw4/prime.hs
@@ -1,14 +1,22 @@
 module PrimeSieve where
 
 import Data.List
+import qualified Data.HashSet as HashSet
+import qualified Data.IntSet as IntSet
 
-comp :: [Integer] -> [Integer] -> [Integer]
+comp :: (Num a, Ord a) => [a] -> [a] -> [a]
 comp xs ys = [ x + y + 2 * x * y | x <- xs, y <- ys, x <= y ]
 
 
 sieveSundaram :: Integer -> [Integer]
-sieveSundaram n = map (\x -> x * 2 + 1) ([1..(2*n+2)] \\ cross n)
+sieveSundaram n = map (\x -> x * 2 + 1) ([1..n] \\ cross n)
     where cross n = comp [1..n] [1..n]
+    -- The operator "\\" has complexity $O(nm)$, which makes it $O(n^3) and is terrible for large input, but anyway...
 
--- The operator "\\" has complexity $O(nm)$, which is terrible for large input, but anyway...
+sieveSundaram' n = map (\x -> x * 2 + 1) . filter (\x -> not $ HashSet.member x (cross n)) $ [1..n]
+    where cross n = HashSet.fromList $ comp [1..n] [1..n]
+    -- Note: HashSet required.
+
+sieveSundaram'' n = map (\x -> x * 2 + 1) . filter (\x -> not $ IntSet.member x (cross n)) $ [1..n]
+    where cross n = IntSet.fromList $ comp [1..n] [1..n]
 
